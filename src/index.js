10617b1d6fe8c516eef9e838e1b121c0d689b974
DanielAllali <daniel.allali12@example.com>
2024-11-17 15:19:50 +0200

    17/11

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./components/store";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <HelmetProvider>
+                <App />
+            </HelmetProvider>
         </Provider>
     </React.StrictMode>
 );
